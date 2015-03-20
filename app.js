c2f99372d6c6328786f990b991156cf51649728c
davidchiou <davidchiou@outlook.com>
2015-03-21 07:59:15 +0800

    計算機

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
-$(function () { //$延緩執行
-	
-
-	var currentText = '';
-	
-	$('#btn1').on('click',function() { //when click 執行function()
-		//alert('hello jquery');
-		//$('#message').text('吃午餐');
-		//$('#message').addClass('green');
-		currentText = currentText + '1';
-		render();
-	})
-
-	$('#btn2').on('click',function() { //when click 執行function()
-		currentText = currentText + '2';
-		render();
-	})
-
-	$('#btnclear').on('click',function() { //when click 執行function()
-		currentText = '';
-		render();
-	})
-
-	function render(){
-		$('#message').text(currentText);
-	}
+$(document).ready(function(){
+	var testNumLength = function(number) {
+        if (number.length > 9) {
+            totaldiv.text(number.substr(number.length-9,9));
+            if (number.length > 15) {
+                number = "";
+                totaldiv.text("Err");
+            }
+        } 
+    };
+	var number = "";
+    var newnumber = "";
+    var operator = "";
+    var totaldiv = $("#total");
+    totaldiv.text("0");
+    $("#numbers a").not("#clear,#clearall").click(function(){
+		number += $(this).text();
+		totaldiv.text(number);
+		testNumLength(number);
+    });
+    $("#operators a").not("#equals").click(function(){
+		operator = $(this).text();
+		newnumber = number;
+		number = "";
+		totaldiv.text("0");
+    });
+    $("#clear,#clearall").click(function(){
+		number = "";
+		totaldiv.text("0");
+		if ($(this).attr("id") === "clearall") {
+			newnumber = "";
+		}
+    });
+    $("#equals").click(function(){
+		if (operator === "+"){
+			number = (parseInt(number, 10) + parseInt(newnumber,10)).toString(10);
+		} else if (operator === "-"){
+			number = (parseInt(newnumber, 10) - parseInt(number,10)).toString(10);
+		} else if (operator === "/"){
+			number = (parseInt(newnumber, 10) / parseInt(number,10)).toString(10);
+		} else if (operator === "*"){
+			number = (parseInt(newnumber, 10) * parseInt(number,10)).toString(10);
+		}
+		totaldiv.text(number);
+		testNumLength(number);
+		number = "";
+		newnumber = "";
+    });
 });
